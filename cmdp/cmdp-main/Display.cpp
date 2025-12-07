1bcf8adb6ce9fcc0b7d23872b3fb315b16671888
Alexandre Agneray <122207242+alexagneray@users.noreply.github.com>
2025-12-07 09:31:17 +0100

    Display : buffering & decomposition par ligne

--- cmdp/cmdp-main/Display.cpp
+++ cmdp/cmdp-main/Display.cpp
@@ -2,11 +2,15 @@
 #include "Display.h"
 
 
+#define CLEAR_CONSOLE_LINE "                                                                                                   "
+#define CLEAR_CONSOLE_LINE_SIZE 100
+
 void Display::RefreshBuffer()
 {
     std::unique_lock<std::mutex> lock(m_mutStrBuffer);
 	std::stringstream ss;
     for (const auto& pair : m_mapVal) {
+        m_mapValRefresh[pair.first] = false;
         ss << pair.first << " = ";
         if (pair.second.type() == typeid(int)) {
             ss << std::any_cast<int>(pair.second);
@@ -23,24 +27,74 @@ void Display::RefreshBuffer()
         else {
             ss << "unknown type";
         }
-        ss << std::endl;
+        std::string strLine = ss.str();
+        std::size_t hash = std::hash<std::string>{}(strLine);
+
+        if (m_mapValHash[pair.first] != hash)
+        {
+            m_mapValHash[pair.first] = hash;
+            m_mapValDisplay[pair.first] = strLine;
+            m_mapValRefresh[pair.first] = true;
+            
+        }
+        ss.str("");
+        ss.clear();
     }
     m_strBuffer = ss.str();
+
+}
+
+Display::Display() :
+    m_bDoRefresh(false),
+    m_uLastHash(0)
+{
 }
 
 void Display::Show()
 {
-	std::unique_lock<std::mutex> lock(m_mutStrBuffer);
+    std::unique_lock<std::mutex> lock(m_mutStrBuffer);
     HANDLE m_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(m_hConsole, {0,0});
-    DWORD dwCharsWritten = 0;
-    WriteConsoleA(
-        m_hConsole,
-        m_strBuffer.c_str(),
-        (DWORD)m_strBuffer.length(),
-        &dwCharsWritten,
-        NULL
-    );
+
+    short i = 0;
+    for (auto it = m_mapValDisplay.begin();
+        it!=m_mapValDisplay.end();
+        it++)
+    {
+        if (!m_mapValRefresh[it->first])
+        {
+            i++;
+            continue;
+        }
+        SetConsoleCursorPosition(m_hConsole, { 0,i });
+
+        DWORD dwCharsWritten = 0;
+        WriteConsoleA(m_hConsole,
+            CLEAR_CONSOLE_LINE,
+            CLEAR_CONSOLE_LINE_SIZE,
+            &dwCharsWritten,
+            NULL);
+        SetConsoleCursorPosition(m_hConsole, { 0,i });
+        WriteConsoleA(
+            m_hConsole,
+            it->second.c_str(),
+            (DWORD)it->second.length(),
+            &dwCharsWritten,
+            NULL
+        );
+        i++;
+    }
+    
+	//std::unique_lock<std::mutex> lock(m_mutStrBuffer);
+ //   HANDLE m_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+ //   SetConsoleCursorPosition(m_hConsole, {0,0});
+ //   DWORD dwCharsWritten = 0;
+ //   WriteConsoleA(
+ //       m_hConsole,
+ //       m_strBuffer.c_str(),
+ //       (DWORD)m_strBuffer.length(),
+ //       &dwCharsWritten,
+ //       NULL
+ //   );
 }
 
 void Display::Set(const std::string&& name, const std::any&& val)
--- cmdp/cmdp-main/Display.h
+++ cmdp/cmdp-main/Display.h
@@ -8,10 +8,16 @@ private:
 
 	std::mutex m_mutMapVal;
 	std::map<std::string, std::any> m_mapVal;
+	std::map<std::string, std::string> m_mapValDisplay;
+	std::map<std::string, std::size_t> m_mapValHash;
+	std::map<std::string, bool> m_mapValRefresh;
+
+	std::size_t m_uLastHash;
+	bool m_bDoRefresh;
 
 	void RefreshBuffer();
 public:
-	
+	Display();
 	void Show();
 	void Set(const std::string&& name, const std::any&& val);
 };
